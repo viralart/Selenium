93ca42283923fab120640fb5bf61d8681bb1cead
Viral Not QA <viral.maurya@artoon.in>
2025-07-07 15:33:11 +0530

    Commit

---
+++ swapping.java
@@ -0,0 +1,10 @@
+public class swapping {
+    public static void main(String args[]){
+        int a = 10, b =20, temp;
+        temp = a;
+        a = b;
+        b = temp;
+        System.out.println(a+" "+b);
+    }
+    
+}
