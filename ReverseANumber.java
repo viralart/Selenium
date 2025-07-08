daed8ec223b2b49382853e6243b74d15588282ca
Viral Not QA <viral.maurya@artoon.in>
2025-07-08 12:36:32 +0530

    Commit

---
+++ ReverseANumber.java
@@ -0,0 +1,9 @@
+public class ReverseANumber {
+    public static void main(String args[]){
+        int number = 120;
+        for(int i=1;i<number;i++){
+
+        }
+        System.out.println();
+    }
+}
