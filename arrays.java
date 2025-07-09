b87ad4fe4df2e46705030057fa88ccaa0aeef797
Viral Not QA <viral.maurya@artoon.in>
2025-07-09 10:23:37 +0530

    Commit

--- arrays.java
+++ arrays.java
@@ -11,8 +11,9 @@ public class arrays {
         System.out.println(arr2.length);
         for(int i=0;i< arr2.length;i++){
             for(int j=0;j< arr2[i].length;j++){
-                System.out.println(arr2[i][j]);
+                System.out.print(arr2[i][j]+"  ");
             }
+            System.out.println();
         }
     } 
 }
