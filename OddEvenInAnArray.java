c27536897bc1d14e0a6f310bdb0a6ad1b27783e6
Viral Not QA <viral.maurya@artoon.in>
2025-07-11 10:33:35 +0530

    Commit

--- OddEvenInAnArray.java
+++ OddEvenInAnArray.java
@@ -1,11 +1,23 @@
 public class OddEvenInAnArray {
     public static void main(String args[]){
-        int arr[] = {1,2,3,4,5}, even=0, odd=0;
-        for(int i= 0;i<arr.length;i++){
-            if(arr[i]%2==0){
-                even++;
-            }else{
-                odd++;
+        // int arr[] = {1,2,3,4,5}, even=0, odd=0;
+        // for(int i= 0;i<arr.length;i++){
+        //     if(arr[i]%2==0){
+        //         even++;
+        //     }else{
+        //         odd++;
+        //     }
+        // }
+        // System.out.println("Even "+even+" "+"Odd "+odd);
+
+        int arr[][] ={{1,2},{3,4},{5,6}}, even=0,odd=0;
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[i].length;j++){
+                if(arr[i][j]%2==0){
+                    even++;
+                }else{
+                    odd++;
+                }
             }
         }
         System.out.println("Even "+even+" "+"Odd "+odd);
--- SumInArray.java
+++ SumInArray.java
@@ -1,8 +1,16 @@
 public class SumInArray {
     public static void main(String[] args) {
-        int arr[] = {1,2,3},res=0;
+        // int arr[] = {1,2,3},res=0;
+        // for(int i=0;i<arr.length-1;i++){
+        //     res = arr[i] + arr[i+1];
+        // }
+        // System.out.println(res);
+
+        int arr[][] ={{1,2},{3,4},{5,6}}, res=0;
         for(int i=0;i<arr.length-1;i++){
-            res = arr[i] + arr[i+1];
+            for(int j=0;j<arr[i].length-1;j++){
+                res = arr[i][j] + arr[i+1][j+1];
+            }
         }
         System.out.println(res);
     }
