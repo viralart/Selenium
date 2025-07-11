14bb5f5736d52f8b2595ceee46113de3c75257b7
Viral Not QA <viral.maurya@artoon.in>
2025-07-11 18:12:24 +0530

    Commit

--- DuplicateNumbers.java
+++ DuplicateNumbers.java
@@ -1,10 +1,20 @@
 public class DuplicateNumbers {
     public static void main(String args[]){
-        int arr[]={100,200,300,400,500,100,600,700,100,800,900};
-        int count=0;
-        for(int i=0;i<arr.length-1;i++){
-            if(arr[i] == 100){
-                count++;
+        // int arr[]={100,200,300,400,500,100,600,700,100,800,900};
+        // int count=0;
+        // for(int i=0;i<arr.length-1;i++){
+        //     if(arr[i] == 100){
+        //         count++;
+        //     }
+        // }
+        // System.out.println(count);
+
+        int arr[][] = {{100,200},{300,400},{500,600},{100,700},{800,100}}, count =0;
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[i].length;j++){
+                if(arr[i][j]==100){
+                    count++;
+                }
             }
         }
         System.out.println(count);
---
+++ LinearSearchAlgorithm.java
@@ -0,0 +1,15 @@
+public class LinearSearchAlgorithm {
+    public static void main(String args[]){
+        // int arr[] = {10,20,30,40,50};
+        // int search =40;
+        // for(int i=0;i<arr.length;i++){
+        //     if(arr[i]==search){
+        //         System.out.println("Found");
+        //         break;
+        //     }else{
+        //         System.out.println("Not Found");
+        //         break;
+        //     }
+        // }
+    }
+}
