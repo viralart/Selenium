ae82d545f8354801384a3d24025ef7e60ca680a7
Viral Not QA <viral.maurya@artoon.in>
2025-07-14 18:12:21 +0530

    Commit

---
+++ Sorting.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class Sorting {
+    public static void main(String args[]){
+        int arr[] = {100,600,300,200,500,400};
+        // for(int i:arr){
+        //     System.out.println(i);
+        // }
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
